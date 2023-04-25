ff6c8304e07d7dcddb770ea64f4b96d27144064b
baolean <baolean@protonmail.com>
2023-04-26 04:51:43 +0800

    Add symtest examples from the post

---
+++ SymTest/test/MiniVat.t.sol
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/MiniVat.sol";
+
+contract MiniVatTest is Test {
+    MiniVat public vat;
+
+    function setUp() public {
+        vat = new MiniVat();
+        vat.init();
+    }
+
+    function testInvariant(int x) public {
+        vat.frob(10 ** 18);
+        vat.fold(-10 ** 27);
+        vat.init();
+        (uint Art, uint rate, uint debt) = vat.getValues();
+
+        assertEq(vat.debt(), vat.Art() * vat.rate());
+    }
+
+    function testInvariant_k(int x) public {
+        setUp();
+
+        vat.frob(10 ** 18);
+        vat.fold(-10 ** 27);
+        vat.init();
+        (uint Art, uint rate, uint debt) = vat.getValues();
+
+        assertEq(vat.debt(), vat.Art() * vat.rate());
+    }
+
+    function proveInvariant() public {
+        vat.frob(10 ** 18);
+        vat.fold(-10 ** 27);
+        vat.init();
+
+        (uint Art, uint rate, uint debt) = vat.getValues();
+
+        assertEq(vat.debt(), vat.Art() * vat.rate());
+    }
+}
--- SymTest/test/PostExample_2tx.t.sol
+++ SymTest/test/PostExample_2tx.t.sol
@@ -2,25 +2,25 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/PostExample.sol";
+import "../src/PostExample_2tx.sol";
 
-contract PostExampleTest is Test {
+contract PostExampleTwoTest is Test {
     PostExample public example;
 
     function setUp() public {
         example = new PostExample();
+        // example.setLive(true);
     }
 
     function testBackdoor(uint256 x) public {
+        // (bool isLive, uint256 x)
+        // example.setLive();
         example.backdoor(x);
     }
 
-    function testBackdoor_k(uint256 x) public {
-        setUp();
-        example.backdoor(x);
-    }
-
-    function proveBackdoor(uint256 x) public view {
+    function proveBackdoor(uint256 x) public {
+        // (bool isLive, uint256 x)
+        // example.setLive(isLive);
         example.backdoor(x);
     }
 }
